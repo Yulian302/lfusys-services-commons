9c6ca64eacad63b53a537c71c22e27915d864776
Yulian302 <bohomolyulian3022003@gmail.com>
2025-12-19 18:57:30 +0100

    test: integrate jwt token inclusion into mock request

--- test/utils.go
+++ test/utils.go
@@ -7,14 +7,23 @@ import (
 	"strings"
 	"testing"
 
+	testjwt "github.com/Yulian302/lfusys-services-commons/test/jwt"
 	"github.com/gin-gonic/gin"
 )
 
-func PerformRequest(r *gin.Engine, t *testing.T, method, url string, body io.Reader, headers []string) *httptest.ResponseRecorder {
+func PerformRequest(r *gin.Engine, t *testing.T, method, url string, body io.Reader, headers []string, withJwt bool, securityKey, subject string) *httptest.ResponseRecorder {
 	w := httptest.NewRecorder()
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	if withJwt {
+		req.AddCookie(&http.Cookie{
+			Name:  "jwt",
+			Value: testjwt.MakeTestJWT(t, securityKey, subject),
+			Path:  "/",
+		})
 	}
 
 	for _, h := range headers {

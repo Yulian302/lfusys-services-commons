71deb98c64c494295f3dfe4ca4bb590aa6d92483
Yulian302 <bohomolyulian3022003@gmail.com>
2025-12-21 20:17:08 +0100

    ref: improve error handling

---
+++ errors/domain.go
@@ -0,0 +1,22 @@
+package errors
+
+import "errors"
+
+var (
+	ErrUserNotFound      = errors.New("user not found")
+	ErrUserAlreadyExists = errors.New("user already exists")
+	ErrInvalidToken      = errors.New("invalid token")
+	ErrInvalidTokenType  = errors.New("invalid token type")
+	ErrTokenSignature    = errors.New("could not sign token")
+	ErrTokenCreation     = errors.New("could not create token")
+
+	ErrInternalServer = errors.New("internal server error")
+
+	// uploads
+	ErrFileSizeExceeded = errors.New("file size exceeded")
+	ErrFileSizeInvalid  = errors.New("invalid file size")
+	ErrSessionConflict  = errors.New("session already exists")
+	ErrGrpcFailed       = errors.New("grpc call failed")
+
+	ErrServiceUnavailable = errors.New("service unavailable")
+)

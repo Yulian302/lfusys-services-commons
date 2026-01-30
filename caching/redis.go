a6e4036f89b92e8837314d4604c1c65a6d6fb7a5
Yulian302 <bohomolyulian3022003@gmail.com>
2026-01-30 20:29:44 +0100

    fix: Fix minor errors

--- caching/redis.go
+++ caching/redis.go
@@ -7,6 +7,10 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+const (
+	cacheTimeout = 50 * time.Millisecond
+)
+
 type RedisCachingService struct {
 	client *redis.Client
 }
@@ -18,19 +22,29 @@ func NewRedisCachingService(c *redis.Client) *RedisCachingService {
 }
 
 func (svc *RedisCachingService) Get(ctx context.Context, key string) (string, error) {
+	ctx, cancel := context.WithTimeout(ctx, cacheTimeout)
+	defer cancel()
+
 	val, err := svc.client.Get(ctx, key).Result()
-	if err == redis.Nil {
+	if err != nil {
 		return "", nil
 	}
-	return val, err
+
+	return val, nil
 }
 
 func (svc *RedisCachingService) Set(ctx context.Context, key string, value string, ttl time.Duration) error {
-	cmd := svc.client.Set(ctx, key, value, ttl)
-	return cmd.Err()
+	ctx, cancel := context.WithTimeout(ctx, cacheTimeout)
+	defer cancel()
+
+	_ = svc.client.Set(ctx, key, value, ttl)
+	return nil
 }
 
 func (svc *RedisCachingService) Delete(ctx context.Context, key string) error {
-	cmd := svc.client.Del(ctx, key)
-	return cmd.Err()
+	ctx, cancel := context.WithTimeout(ctx, cacheTimeout)
+	defer cancel()
+
+	_ = svc.client.Del(ctx, key)
+	return nil
 }
